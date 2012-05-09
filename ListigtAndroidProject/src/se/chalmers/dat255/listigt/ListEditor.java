ce12981c205efb10efeaea187f7218a6c02edea9
Erik Olesund <erik@olesund.se>
2012-05-09 18:01:06 +0200

    Removed unused imports

--- ListigtAndroidProject/src/se/chalmers/dat255/listigt/ListEditor.java
+++ ListigtAndroidProject/src/se/chalmers/dat255/listigt/ListEditor.java
@@ -8,12 +8,9 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
 
 public class ListEditor extends Activity implements TextWatcher {
 	EditText editableListTitle; //creates an editable textbox
