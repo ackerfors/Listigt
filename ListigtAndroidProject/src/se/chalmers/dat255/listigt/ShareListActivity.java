a493d2e10eeb27f34e98f08a2b0e8c056e269a52
Erik Olesund <erik@olesund.se>
2012-05-15 21:06:04 +0200

    Implemented e-mail connection

--- ListigtAndroidProject/src/se/chalmers/dat255/listigt/ShareListActivity.java
+++ ListigtAndroidProject/src/se/chalmers/dat255/listigt/ShareListActivity.java
@@ -3,10 +3,12 @@ package se.chalmers.dat255.listigt;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * This activity will be launched when a user wants to share it's list with friends.
@@ -17,7 +19,6 @@ import android.widget.EditText;
 public class ShareListActivity extends Activity{
 	private EditText email, subject, message;
 	private Long currentRowId;
-	private AlertDialog.Builder builder;
 	private Button sendButton;
 	private Bundle extras;
 	
@@ -28,8 +29,7 @@ public class ShareListActivity extends Activity{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.share_list); 
-		builder = new AlertDialog.Builder(this);
+		setContentView(R.layout.share_list);
 		email = (EditText) findViewById(R.id.emailField);	
 		//email.addTextChangedListener(this); - might want to listen to make sure e-mail adress is correct
 		subject = (EditText) findViewById(R.id.subjectField);
@@ -39,7 +39,7 @@ public class ShareListActivity extends Activity{
 		//confirm button per default disabled
 		sendButton.setEnabled(true);//CHANGE TO FALSE!
 		
-		currentRowId = null;
+		//currentRowId = null;
 		extras = getIntent().getExtras();
 		
 	}
@@ -58,9 +58,34 @@ public class ShareListActivity extends Activity{
      * When the Send-button is clicked, this method runs (set in share_list.xml)
      */
     public void sendList(View v){
-    	//send e-mail stuff. To be implemented by ACKERFORS
-    	showDialog();//Show confirm dialog, and then go back to where we came from
+    	sendTheEmail();
+    	//showDialog();//Show confirm dialog, and then go back to where we came from
+    	onBackPressed();//go back to the MainListActivity
     }
+    
+    private void sendTheEmail(){
+    	Mail m = new Mail("listigtapp@gmail.com", "1337_listigt"); 
+    	String emailString = email.getText().toString();
+    	String subjectString = subject.getText().toString();
+    	String messageString = message.getText().toString();
+        String[] toArr = {emailString, "erik@olesund.se"}; 
+        m.setTo(toArr); 
+        m.setFrom("listigtapp@gmail.com"); 
+        m.setSubject(subjectString);
+        m.setBody(messageString);
+   
+        try {  
+   
+          if(m.send()) { 
+            Toast.makeText(ShareListActivity.this, "Email was sent successfully.", Toast.LENGTH_LONG).show(); 
+          } else { 
+            Toast.makeText(ShareListActivity.this, "Email was not sent.", Toast.LENGTH_LONG).show(); 
+          } 
+        } catch(Exception e) { 
+          Toast.makeText(ShareListActivity.this, "There was a problem sending the email.", Toast.LENGTH_LONG).show(); 
+          //Log.e("ShareListActivity", "Could not send email", e); 
+        } 
+      } 
     
     /**
 	 * Shows a confirm dialog when the list has been shared
@@ -74,7 +99,6 @@ public class ShareListActivity extends Activity{
 			        public void onClick(DialogInterface dialog, int which) {
 			        // Write your code here to execute after dialog closed
 			        dialog.dismiss();
-			        onBackPressed();//go back to the MainListActivity
 			        }
 			});
 			// Showing Alert Message
