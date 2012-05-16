a26971fcf3137f444faeeb1d87b1dcf823d7ba07
Erik Olesund <erik@olesund.se>
2012-05-16 11:58:53 +0200

    Completed the e-mail functionality

--- ListigtAndroidProject/src/se/chalmers/dat255/listigt/ShareListActivity.java
+++ ListigtAndroidProject/src/se/chalmers/dat255/listigt/ShareListActivity.java
@@ -1,10 +1,14 @@
 package se.chalmers.dat255.listigt;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.mail.SendFailedException;
+
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,17 +35,13 @@ public class ShareListActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.share_list);
 		email = (EditText) findViewById(R.id.emailField);	
-		//email.addTextChangedListener(this); - might want to listen to make sure e-mail adress is correct
 		subject = (EditText) findViewById(R.id.subjectField);
 		message = (EditText) findViewById(R.id.messageField);
-		
 		sendButton = (Button) findViewById(R.id.sendButton);
-		//confirm button per default disabled
-		sendButton.setEnabled(true);//CHANGE TO FALSE!
-		
-		//currentRowId = null;
+		//send button per default enabled
+		sendButton.setEnabled(true);	
+		currentRowId = null;
 		extras = getIntent().getExtras();
-		
 	}
 	
 	/**
@@ -58,51 +58,33 @@ public class ShareListActivity extends Activity{
      * When the Send-button is clicked, this method runs (set in share_list.xml)
      */
     public void sendList(View v){
-    	sendTheEmail();
-    	//showDialog();//Show confirm dialog, and then go back to where we came from
-    	onBackPressed();//go back to the MainListActivity
-    }
-    
-    private void sendTheEmail(){
     	Mail m = new Mail("listigtapp@gmail.com", "1337_listigt"); 
     	String emailString = email.getText().toString();
     	String subjectString = subject.getText().toString();
     	String messageString = message.getText().toString();
-        String[] toArr = {emailString, "erik@olesund.se"}; 
-        m.setTo(toArr); 
-        m.setFrom("listigtapp@gmail.com");//has to be the e-mail adress 
-        m.setSubject(subjectString);
-        m.setBody(messageString);
-   
+        List<String> emailList = Arrays.asList(emailString.split("\\s*,\\s*"));
+        String [] emailArr = new String[emailList.size()];
+        emailList.toArray(emailArr);
+        m.setTo(emailArr); 
+        m.setFrom("listigtapp@gmail.com");//has to be an e-mail adress 
+        m.setSubject("List from Listigt: " + subjectString);
+        m.setBody("Message from the creator: " + messageString);
+        System.out.println("TO ARR: " + Arrays.toString(emailArr) + " // emailString: " + emailString);
         try {  
    
           if(m.send()) { 
-            Toast.makeText(ShareListActivity.this, "Your list was shared successfully!", Toast.LENGTH_LONG).show(); 
+            Toast.makeText(ShareListActivity.this, "Your list was shared successfully!", Toast.LENGTH_LONG).show();
+            onBackPressed();//go back to the MainListActivity
           } else { 
-            Toast.makeText(ShareListActivity.this, "Email was not sent.", Toast.LENGTH_LONG).show(); 
+            Toast.makeText(ShareListActivity.this, "Sorry, but the list was not shared. Please try again.", Toast.LENGTH_LONG).show();
           } 
-        } catch(Exception e) { 
-          Toast.makeText(ShareListActivity.this, "There was a problem sending the email.", Toast.LENGTH_LONG).show(); 
-          //Log.e("ShareListActivity", "Could not send email", e); 
+        } catch(SendFailedException e) { 
+          Toast.makeText(ShareListActivity.this, "There was a poblem sharing the list: " + e.getMessage(), Toast.LENGTH_LONG).show();
         } 
+        catch(Exception e){
+        	Toast.makeText(ShareListActivity.this, "The list could not be shared, sorry buddy!", Toast.LENGTH_LONG).show();
+        	Log.e("ShareListActivity", "Could not send email", e);
+        }
       } 
-    
-    /**
-	 * Shows a confirm dialog when the list has been shared
-	 */
-	private void showDialog(){
-		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-			// Setting Dialog Title
-			alertDialog.setTitle("The list has succesfully been shared!");
-			// Setting OK Button
-			alertDialog.setButton("OK, got it!", new DialogInterface.OnClickListener() {
-			        public void onClick(DialogInterface dialog, int which) {
-			        // Write your code here to execute after dialog closed
-			        dialog.dismiss();
-			        }
-			});
-			// Showing Alert Message
-			alertDialog.show();
-	}
 }
 	
